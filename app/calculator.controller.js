b3c86d740b99ab800f2278f7731cdb1c4cf7761e
Sandeepp Bajaj <sandeepp.bajaj@gmail.com>
2016-04-28 17:36:09 -0700

    Pass test for sum and additional browser (Firefox)

---
+++ app/calculator.controller.js
@@ -0,0 +1,5 @@
+angular.module('calculatorApp', []).controller('CalculatorController', function CalculatorController($scope) {
+  $scope.sum = function() {
+    $scope.z = $scope.x + $scope.y;
+  };
+});
